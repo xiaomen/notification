b7a012aad344bc0b180cad428861d31fc5417937
luoruici <none@none>
2012-02-09 23:06:38 +0800

    implement a simple demo of notification service

---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+import web
+
+from gevent.queue import Queue 
+from jinja2 import Environment, FileSystemLoader
+
+urls = ('/long', 'long_polling',
+        '/.*', 'index')
+
+jinja_env = Environment(
+    loader=FileSystemLoader(os.path.join(os.path.dirname(__file__),
+                            'templates')),
+    extensions=[])
+jinja_env.globals.update({})
+
+message_queue = Queue()
+
+class index:
+
+    def GET(self):
+        return jinja_env.get_template('index.html').render()
+
+    def POST(self):
+        user_data = web.input()
+        message_queue.put(user_data['message'])
+        return web.seeother('/')
+
+class long_polling:
+
+    def GET(self):
+        message = message_queue.get()
+        return message
+
+
+app = web.application(urls, globals())
+wsgi_app = app.wsgifunc()
+
+if __name__ == "__main__":
+    app.run()
